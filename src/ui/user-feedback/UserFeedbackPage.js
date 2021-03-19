9384c5983fae89cd4fea061c3676e56057abaf48
Giuseppe de Santis <gdesantis@gmail.com>
2021-03-20 00:38:24 +0100

    wip

--- src/ui/user-feedback/UserFeedbackPage.js
+++ src/ui/user-feedback/UserFeedbackPage.js
@@ -1,37 +1,45 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import withPermissions from 'ui/auth/withPermissions';
 import InternalPage from 'ui/internal-page/InternalPage';
 import { ADMINISTRATION_AREA_PERMISSION } from 'state/permissions/const';
 
-export const UserFeedbackPageBody = ({ userPermissions }) => {
-  console.log({ userPermissions });
+export const UserFeedbackPageBody = () => {
+  // console.log({ userPermissions });
 
   const [commentsEnabled, setCommentsEnabled] = useState(false);
   const toggleComments = () => setCommentsEnabled(!commentsEnabled);
+
+  const handleOverlayClick = (e) => {
+    console.log(e.nativeEvent);
+  };
 
   return (
     <InternalPage className="DashboardPage">
       <button onClick={toggleComments}> Toggle Comments</button>
       <div>Comments are <b>{commentsEnabled ? 'enabled' : 'disabled'}</b></div>
+      {commentsEnabled && (<div
+        style={{
+          position: 'absolute',
+          zIndex: '1',
+          background: 'red',
+          opacity: '0.2',
+          width: '100%',
+          height: '800px',
+        }}
+        onClick={handleOverlayClick}
+      />)}
       <iframe
         title="preview"
         id="previewFrame"
-        src="http://localhost:8080/entando-de-app/preview/en/my_homepage"
+        src="http://localhost:8080/entando-de-app/en/my_homepage.page"
         width="100%"
         height="800px"
         onError={e => console.warn('*************', e)}
       />
     </InternalPage>
   );
-};
-
-UserFeedbackPageBody.propTypes = {
-  userPermissions: PropTypes.arrayOf(PropTypes.string),
-};
-
-UserFeedbackPageBody.defaultProps = {
-  userPermissions: [],
 };
 
 export default withPermissions(ADMINISTRATION_AREA_PERMISSION)(UserFeedbackPageBody);
